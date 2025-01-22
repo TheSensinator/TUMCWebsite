f195387bdf570ab67bb099ac0b4f730019120b71
TheSensinator <56374210+TheSensinator@users.noreply.github.com>
2025-01-21 23:09:37 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -32,5 +32,5 @@ app.use(
 app.use('/api/admin', adminRouter);
 app.use('/api/auth', authRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
