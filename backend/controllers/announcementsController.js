d1ebe6aed431a84edf9d5a73d9de9e5c57457f7b
TheSensinator <56374210+TheSensinator@users.noreply.github.com>
2025-01-24 23:03:56 -0600

    Update announcementsController.js

--- backend/controllers/announcementsController.js
+++ backend/controllers/announcementsController.js
@@ -21,12 +21,24 @@ exports.updateAnnouncements = (req, res) => {
     }
 
     // Update the fields that were provided
-    if (livestreamLink) announcements.livestreamLink = livestreamLink;
-    if (announcement1) announcements.announcement1 = announcement1;
-    if (announcement2) announcements.announcement2 = announcement2;
-    if (announcement3) announcements.announcement3 = announcement3;
-    if (announcement4) announcements.announcement4 = announcement4;
-    if (announcement5) announcements.announcement5 = announcement5;
+    if (livestreamLink !== undefined) {
+        announcements.livestreamLink = livestreamLink || announcements.livestreamLink;
+    }
+    if (announcement1 !== undefined) {
+        announcements.announcement1 = announcement1 || announcements.announcement1;
+    }
+    if (announcement2 !== undefined) {
+        announcements.announcement2 = announcement2 || announcements.announcement2;
+    }
+    if (announcement3 !== undefined) {
+        announcements.announcement3 = announcement3 || announcements.announcement3;
+    }
+    if (announcement4 !== undefined) {
+        announcements.announcement4 = announcement4 || announcements.announcement4;
+    }
+    if (announcement5 !== undefined) {
+        announcements.announcement5 = announcement5 || announcements.announcement5;
+    }
 
     res.status(200).json({
         message: "Announcements updated successfully!",
