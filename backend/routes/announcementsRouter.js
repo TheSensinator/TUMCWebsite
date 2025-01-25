4ce869660c0fd55269ed471e6f5943d2c92cf6c6
TheSensinator <56374210+TheSensinator@users.noreply.github.com>
2025-01-24 23:07:32 -0600

    Update announcementsRouter.js

--- backend/routes/announcementsRouter.js
+++ backend/routes/announcementsRouter.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const announcementsController = require('../controllers/announcementsController');
 
-// Handle the routes related to announcements
 router.get('/', announcementsController.getAnnouncements);
 router.put('/', announcementsController.updateAnnouncements);
 
