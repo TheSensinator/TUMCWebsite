e8840360deba19309712acf13c443b51318458a1
TheSensinator <56374210+TheSensinator@users.noreply.github.com>
2025-01-25 15:44:35 -0600

    Update Announcements.js

--- backend/models/Announcements.js
+++ backend/models/Announcements.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 
 const AnnouncementsSchema = new mongoose.Schema({
-  livestreamLink: { type: String, required: true },
-  announcement1: { type: String, default: "" },
-  announcement2: { type: String, default: "" },
-  announcement3: { type: String, default: "" },
-  announcement4: { type: String, default: "" },
-  announcement5: { type: String, default: "" },
+  livestreamLink: { type: String, required: false },
+  announcement1: { type: String, default: "", required: false },
+  announcement2: { type: String, default: "", required: false },
+  announcement3: { type: String, default: "", required: false },
+  announcement4: { type: String, default: "", required: false },
+  announcement5: { type: String, default: "", required: false },
 });
 
 module.exports = mongoose.model("Announcement", AnnouncementsSchema);
