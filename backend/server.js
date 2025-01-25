47c366297824828bdb34fe5d08b5356f6e767b31
TheSensinator <56374210+TheSensinator@users.noreply.github.com>
2025-01-25 14:58:06 -0600

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,7 @@ const authRoutes = require('./routes/authRoutes');
 const adminRouter = require('./routes/admin');
 const announcementsRouter = require('./routes/announcementsRouter');
 const app = express();
-const mongoose = require("mongoose");
+const mongoose = require("mongodb");
 
 const MONGO_URI = process.env.MONGO_URI;
 
